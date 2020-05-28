c60b60311e55c9f7b9c266221c807e1d5a531246
rauljgarcia <rauljgarcia97@gmail.com>
2020-05-28 17:02:23 -0600

    bit positions in input words

--- cExer.c
+++ cExer.c
@@ -2,10 +2,58 @@
 
 int main()
 {	
-	/*increasing bit vector size from 8 to 16-bit using left shift*/
-    int a = 0xaa;
+	/*increasing bit vector size from 8 to 16-bit using left shift
+	int a = 0xaa;
 	int b = a<<8;
-	printf("%x\n",b);
+	*/
+
+	/* returning 16 bit word from 8, all even-numbered bits are set to 1 
+	int a = 0x55;
+	int b = a<<8;
+	int c = b | a;
+	int d = c<<16;
+	int e = d | c;
+	*/
+	
+	/* check if any odd-numbered bits in a 32 bit word are set to 1
+	int anyOddBit(int x){
+	int a = 0xaa;
+	int	b = a<<8;
+	int c = b | a;
+	int d = c << 16;
+	int e = d | c;
+
+	int z = !(!(x & e));
+	} */
+
+
+	/* check if all even-numbered bits set to 1
+	int allEvenbits(x)
+	int a = 0x55;
+	int	b = a<<8;
+	int c = b | a;
+	int d = c << 16;
+	int e = d | c;
+
+	int z = !(~x & e);
+	printf("%x\n",z);
+	} */
+
+	/*
+	1111 1111 1111 1111 1111 1111 1111 1110
+	0101 0101 0101 0101 0101 0101 0101 0101
+
+	0101 0101 0101 0101 0101 0101 0101 0101 
+	1111 1111 1111 1111 1111 1111 1111 1111 */
+
+	/*
+	0001 0010 0011 0100 
+	0001 0010 0000 0000 */
+
+	 
+	
+
+
     
 	return 0;
 }
